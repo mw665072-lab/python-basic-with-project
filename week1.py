756eba16ef4b76dcfd236c2e17269b58fe4c15f6
waqas-smrtbooking <waqas@smrtbooking.com>
2025-10-22 11:02:57 +0500

    python basic concepts

--- week1.py
+++ week1.py
@@ -9,141 +9,141 @@ import re
 # user_input = input("Please Enter Your Name")
 # print(" Hello",user_input,"! Welcome to Journey of Machine Learning.")
 
-# number_1 = int(input("Enter a number: "))
-# number_2 = int(input("Enter another number: "))
-# print("The sum of", number_1,"And",number_2,"Is",number_1+number_2)
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter another number: "))
+print("The sum of", number_1,"And",number_2,"Is",number_1+number_2)
 
 
-# length = int(input("Enter the length"))
-# width = int(input("Enter the width"))
-# print("Area of rectangle is",width * length)
+length = int(input("Enter the length"))
+width = int(input("Enter the width"))
+print("Area of rectangle is",width * length)
 
-# a = 10
-# b = 5.5
-# c = "Hello"
-# d = True
-#
-# print(type(a))
-# print(type(b))
-# print(type(c))
-# print(type(d))
+a = 10
+b = 5.5
+c = "Hello"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
 
 
 
 # Next Round
-# a = 10
-# b = 20
-#
-# a = a + b
-# b = a - b
-# a = a - b
-# print(a,b)
+a = 10
+b = 20
+
+a = a + b
+b = a - b
+a = a - b
+print(a,b)
 
 
-# number_1 = 10
-# number_2 = 20
-# number_3 = 30
-# number_4 = 40
-#
-# avarage = number_1 + number_2 + number_3
-# print(avarage/ 4)
+number_1 = 10
+number_2 = 20
+number_3 = 30
+number_4 = 40
+
+avarage = number_1 + number_2 + number_3
+print(avarage/ 4)
 
 
-# c = 25
-# result = (c *9/5) + 32
-# print(result)
+c = 25
+result = (c *9/5) + 32
+print(result)
 
-# number_1 = 17
-# number_2 = 3
-# print(number_1 % number_2)
-# print(number_1 // number_2)
-# print(number_1 % number_2)
-# print(number_1 + number_2)
-# print(number_1 - number_2)
-# print(number_1 * number_2)
-# print(number_1 / number_2)
+number_1 = 17
+number_2 = 3
+print(number_1 % number_2)
+print(number_1 // number_2)
+print(number_1 % number_2)
+print(number_1 + number_2)
+print(number_1 - number_2)
+print(number_1 * number_2)
+print(number_1 / number_2)
 
 
 
 # Next Round
 
 number = -7
-# if number % 2 == 0:
-#     print("Number is even")
-# else:
-#     print("Number is odd")
+if number % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
 
-# if number < 0:
-#     print("Number is negative")
-# if number > 0:
-#     print("Number is positive")
-# if number == 0:
-#     print("Number is zero")
+if number < 0:
+    print("Number is negative")
+if number > 0:
+    print("Number is positive")
+if number == 0:
+    print("Number is zero")
 
 
 
-# Email = "waqasishaq800@gmail.com"
-# password = "12345678"
-# user_email = input("Enter your email: ")
-# user_password = input("Enter your password: ")
-# if Email == user_email and password == user_password:
-#     print("Login Successful")
-# else:
-#     print("Login Failed")
+Email = "waqasishaq800@gmail.com"
+password = "12345678"
+user_email = input("Enter your email: ")
+user_password = input("Enter your password: ")
+if Email == user_email and password == user_password:
+    print("Login Successful")
+else:
+    print("Login Failed")
 
 
 # Next Round
-# for i in range(10):
-#     print(i)
+for i in range(10):
+    print(i)
 
 
-# enter_table_number = int(input("Enter a number: "))
-# for i in range(1,11):
-#     print(enter_table_number,"*", i ,"=",enter_table_number*i)
+enter_table_number = int(input("Enter a number: "))
+for i in range(1,11):
+    print(enter_table_number,"*", i ,"=",enter_table_number*i)
 
 
-# sum = 0
-# for i in range(enter_table_number+1):
-#     sum = sum + i
-# print("The sum is: " + str(sum))
+sum = 0
+for i in range(enter_table_number+1):
+    sum = sum + i
+print("The sum is: " + str(sum))
 
 
-# count = 0
-# for i in range(enter_table_number+1):
-#     count = count + 1
-# print(count)
+count = 0
+for i in range(enter_table_number+1):
+    count = count + 1
+print(count)
 
-# i="*"
-# for i in range(enter_table_number+1):
-#     print('*' * i)
+i="*"
+for i in range(enter_table_number+1):
+    print('*' * i)
 
 # Next Chapter
-# word = "Hello12"
-# count =0
-# for _ in word:
-#     count += 1
-# # print(count)
+word = "Hello12"
+count =0
+for _ in word:
+    count += 1
+print(count)
 
 
-# word = "Hello world"
-# vowels = "aeiouAEIOU"
-# count = 0
-#
-# for ch in word:
-#     if ch in vowels:
-#         count += 1
-#
-# print("Vowel count:", count)
+word = "Hello world"
+vowels = "aeiouAEIOU"
+count = 0
 
-# word = "python"
-# print(word[::-1])
-#
-# word = "Python"
-# print(word.upper())
-# print(word.lower())
+for ch in word:
+    if ch in vowels:
+        count += 1
 
-# word = "Hello world in the journey of machine learning"
-# print(word.replace(" ","_"))
+print("Vowel count:", count)
+
+word = "python"
+print(word[::-1])
+
+word = "Python"
+print(word.upper())
+print(word.lower())
+
+word = "Hello world in the journey of machine learning"
+print(word.replace(" ","_"))
 
 
 
